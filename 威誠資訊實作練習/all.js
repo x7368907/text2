25f2b3db0aab9c28804f243ec0ea20228b6e6896
x7368907 <108885586+x7368907@users.noreply.github.com>
2023-03-05 20:30:40 +0800

    Add files via upload

---
+++ 威誠資訊實作練習/all.js
@@ -0,0 +1,8 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      rtl: true,
+      touch:true
+    });
+  });
